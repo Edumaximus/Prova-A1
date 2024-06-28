831a1889ae4d4a11b54ebb68d5a5e0168fcf2f67
Eduardo Zaduski <eduardozaduski@gmail.com>
2024-06-28 10:09:03 -0300

    Criação de models

---
+++ my-app/src/models/Tarefa.ts
@@ -0,0 +1,9 @@
+export interface Tarefa{
+    id?: string;
+    titulo: string;
+    descricao: string;
+    criadoEm?: string;
+    categoria: string;
+    categoriaId: string;
+    status: string;
+}

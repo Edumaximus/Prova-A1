4617b9b6ac240eebdde44fd9f7be7c69b6e4f2ea
Eduardo Zaduski <eduardozaduski@gmail.com>
2024-06-28 10:48:50 -0300

    Adição de database

--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af849e61b566cad6d618e1db357be33e0d228d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5407ea16d86f718ae2138a0e5b469445abad01cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
